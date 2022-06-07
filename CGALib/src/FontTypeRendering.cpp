8789122ee4defa7020021306e7ca34ddb4020174
spiderzazu <92601236+spiderzazu@users.noreply.github.com>
2022-06-07 18:39:28 -0500

    Letras estilo Mario
    Recompilar CGALib

--- CGALib/src/FontTypeRendering.cpp
+++ CGALib/src/FontTypeRendering.cpp
@@ -72,7 +72,7 @@ void FontTypeRendering::Initialize() {
 		return exit(-1);
 	}
 	// Se le indica a dicha cara el archivo ttf que se utilizar�.
-	if (FT_New_Face(ft_lib, "../Fonts/arial.ttf", 0, &face) != 0) {
+	if (FT_New_Face(ft_lib, "../Fonts/SuperMario256.ttf", 0, &face) != 0) {
 		std::cerr << "Unable to load fuente\n";
 		cleanup();
 		return exit(-1);
